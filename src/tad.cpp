c8b220926025690bc5fc872535c2d6d171302993
juan beteta <bttajuan@gmail.com>
2019-12-18 17:28:40 +0100

    segundo commit correcto

--- src/tad.cpp
+++ src/tad.cpp
@@ -19,6 +19,17 @@ main(void)
 		cout << "false" << endl;
 	}
 	
+	TComplejo d(0,0);
+	TComplejo e(1,0);
+	TComplejo f;
+	
+	f = d + e;
+
+	cout << "f == d + e  --> true" << endl;
+	if((d.Re() + e.Re()) == f.Re() && (d.Im() + e.Im()) == f.Im())
+		cout << "true" << endl;
+	else
+		cout << "false" << endl;
 
 	return 0;
 }
